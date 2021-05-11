91352643f5b1cca2627c027c5e397d4277bfa247
SHADAB-HAMIRANI <shadabhamirani@gmail.com>
2021-05-11 14:43:27 +0500

    lets begin

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+home:Scaffold(
+  body: Text("HI THIS IS SHADAB HAMIRANI LEARNING FLUTTER  AND MY EMAIL ID IS :shadabhamirani@gmail.com"),
+
+),
+    );
+  
+     }
+}
